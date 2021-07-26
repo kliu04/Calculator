59a3659ffa962f82ca276a2ee9c5449f74b97060
Kevin Liu <kevinliu04@gmail.com>
2021-07-26 16:18:01 -0400

    basic design

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    return a / b;
+}
+
+function neg(a) {
+    return -a;
+}
+
+function per(a) {
+    return a / 100;
+}
+
+function operate(op, a, b = 1) {
+
+}
